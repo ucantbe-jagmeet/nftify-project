532e1fa743c7bb9bafcdd321013a6e92e8d50218
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-08-25 12:56:55 +0530

    vite react app setup with tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h2 className="bg-red-50">Hello World- Tailwind css setup with vite</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
